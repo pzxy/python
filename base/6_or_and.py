f81ba0000d15f30c2783b57cb0888ec8c24f44be
pzxy <wonkung@163.com>
2022-02-09 22:07:32 +0800

    base

---
+++ base/6_or_and.py
@@ -0,0 +1,8 @@
+print(1 == 2 and 3 > 1)
+print(1 != 2 and 1)
+print(0 and 8)
+print("" and 8)
+print(1 and 8)
+print(6 or 9)
+print(0 or 9)
+# 优先级 not and in
---
+++ base/7_bin_oct.py
@@ -0,0 +1,16 @@
+i = 15
+print(bin(i))
+print(oct(i))
+print(hex(i))
+
+ip = "192.168.1.1"
+print(ip.split("."))
+v = 1 + 2
+
+#None,语言中所有None只有一块内存，不开辟新的内存。
+v1 = None
+print(v1)
+v1 = "asd"
+print(v1)
+v1 = 12
+print(v1)
